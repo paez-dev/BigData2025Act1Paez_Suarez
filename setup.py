5e3b7d6daa88748e55d77f01a268dbeaa5db2480
J.C. Páez R. <91022556+paez-dev@users.noreply.github.com>
2025-03-17 21:51:22 +0000

    actualización del nombre en el setup, de la versión y dependencias.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ingestion-bigdata",
-    version="1.0.0",
+    name="data-enrichment-bigdata",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "pandas>=2.0.0",
@@ -10,13 +10,15 @@ setup(
         "openpyxl>=3.1.2",
         "matplotlib>=3.5.0",
         "seaborn>=0.11.2",
-        "numpy>=1.20.0"
+        "numpy>=1.20.0",
+        "lxml>=4.9.0",  # Para procesamiento de XML
+        "html5lib>=1.1" # Para procesamiento de HTML
     ],
     author="Jean Carlos Páez Ramírez y Juliana Maria Peña Suarez",
     author_email="",
     asignatura="Infraestructura y arquitectura para Big Data",
-    tema="Ingestión y procesamiento de Datos",    
-    description="Etapas de ingesta y preprocesamiento de datos para el proyecto integrador de infraestructura y arquitectura para Big Data",
+    tema="Proceso Completo de Ingesta, Limpieza y Proceso de enriquecimiento de datos",    
+    description="Etapas de ingesta, limpieza y proceso de enriquecimiento de datos para el proyecto integrador de infraestructura y arquitectura para Big Data",
     docente="Andres Felipe Callejas Jaramillo",
     python_requires=">=3.9",
 )
